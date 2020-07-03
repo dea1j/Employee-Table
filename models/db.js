a578358aaa9d2556f63fd17c281f4a12faf0571f
dea1j <jdayo2012@gmail.com>
2020-07-03 05:02:01 +0100

    Employee list CRUD opration

---
+++ models/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/EmployeeDB',
+ {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+ },
+  (err) => {
+    if (!err) { console.log('MongoDB Connection Successful') }
+    else { console.log('Error in DB Connection : ' + err) }
+});
+
+require('./employee.model');
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const db = require('./models/db');
+const employeeController = require('./controllers/employeeController');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+
+const app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyparser.json());
+
+
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+app.use('/employee', employeeController);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
